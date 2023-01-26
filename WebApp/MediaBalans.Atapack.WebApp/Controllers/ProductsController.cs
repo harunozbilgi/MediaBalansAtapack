8c2744ad045208a0468dfec3d2490261f4681d58
harunozbilgi <ozbilgiharun04@gmail.com>
2023-01-26 11:41:33 +0400

    hatalar gonderildi

--- WebApp/MediaBalans.Atapack.WebApp/Controllers/ProductsController.cs
+++ WebApp/MediaBalans.Atapack.WebApp/Controllers/ProductsController.cs
@@ -9,7 +9,7 @@ namespace MediaBalans.Atapack.WebApp.Controllers
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService, 
+        public ProductsController(IProductService productService,
             ICategoryService categoryService)
         {
             _productService = productService;
@@ -24,12 +24,15 @@ namespace MediaBalans.Atapack.WebApp.Controllers
                 lang = HttpContext.Session.GetString("lang");
             ViewBag.Lang = lang;
             ViewData["url"] = slugUrl;
-            var product = await _productService.GetProductsAsync();
+            var product = slugUrl != "all" 
+                ? (await _productService.GetProductsAsync()).Data.Where(x => x.Category.SlugUrl.ToLower() == slugUrl.ToLower())
+                : (await _productService.GetProductsAsync()).Data;
+
             var categories = await _categoryService.GetCategoriesAsync();
 
             return View(new ProductsViewModel
             {
-                Products = product.Data.Take(10).ToList(),
+                Products = product.Take(10).ToList(),
                 Categories = categories.Data.ToList()
             });
         }
@@ -48,21 +51,23 @@ namespace MediaBalans.Atapack.WebApp.Controllers
                 return View(new ProductsViewModel
                 {
                     Product = product.Data,
-                    Products = products.Data.Where(x => x.Id != product.Data.Id).ToList()
+                    Products = products.Data.Where(x => x.Id != product.Data.Id).Take(4).ToList()
                 });
             }
             return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
-        public async Task<PartialViewResult> Filter(int pageIndex, int pageSize)
+        public async Task<PartialViewResult> Filter(int pageIndex, int pageSize, string filterId)
         {
             string lang = "az";
             if (!string.IsNullOrEmpty(HttpContext.Session.GetString("lang")))
                 lang = HttpContext.Session.GetString("lang");
             ViewBag.Lang = lang;
-            var prodcuts = await _productService.GetProductsAsync();
-            return PartialView(prodcuts.Data.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+            var products = (await _productService.GetProductsAsync()).Data.Where(x => x.Category.SlugUrl.ToLower() == filterId.ToLower());
+            return PartialView(products.Skip(pageIndex * pageSize).Take(pageSize).ToList());
         }
+
+
     }
 }
